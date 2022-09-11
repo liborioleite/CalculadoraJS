1d659b79b973ea44fe56fdf427dce5acb3763e70
liborioleite <liborioleite@gmail.com>
2022-09-11 16:13:28 -0300

    primeiro commit de um clone

---
+++ script.js
@@ -0,0 +1,11 @@
+let display = document.getElementById('display').textContent
+let historico = document.getAnimations('display-historico').textContent
+let numerico = document.querySelectorAll('.btn-numero')
+
+
+
+
+function limpar() {
+    display.textContent = ""
+    historico.textContent = ""
+}
